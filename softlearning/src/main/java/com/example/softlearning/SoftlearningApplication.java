878c242926b3cb66d16e9e244277b753caa31a4a
JosepMeseguer <126162197+JosepMeseguer@users.noreply.github.com>
2025-01-22 22:37:33 +0100

    Add files via upload

--- softlearning/src/main/java/com/example/softlearning/SoftlearningApplication.java
+++ softlearning/src/main/java/com/example/softlearning/SoftlearningApplication.java
@@ -1,18 +1,15 @@
 package com.example.softlearning;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class SoftlearningApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(SoftlearningApplication.class, args);
+		SpringApplication.run(SoftlearningApplication.class, args);
 
         System.out.println("\n*****   A P P L I C A T I O N    S T A R T E D   *****\n");
-
 		
 	}
 
